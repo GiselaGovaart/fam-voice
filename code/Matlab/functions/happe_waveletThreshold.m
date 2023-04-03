544cb9c4bd08a517c9ceb9a4eb75b0b9ef8f9143
Gisela Govaart <47251666+GiselaGovaart@users.noreply.github.com>
2023-04-03 10:39:06 +0200

    Update to HAPPE 3.0

--- code/Matlab/functions/happe_waveletThreshold.m
+++ code/Matlab/functions/happe_waveletThreshold.m
@@ -1,6 +1,11 @@
 function EEG = happe_waveletThreshold(EEG,wavThreshold,version)
 %EEG = happe_waveletThreshold(EEG)
-%   Performs wavelet thresholding according to the HAPPE 2.1 pipeline
+%   Performs wavelet thresholding according to the HAPPE 2.1 or 3.0 pipeline
+
+
+% in a between (a previous 3.0) the values for wavLvl were 10, 9, 8, and
+% the 'Wavelet' for 3.0 was 'bior6.8'
+
 
 %% determine wavelet level
 
@@ -12,9 +17,9 @@ if version == 2
     end
 elseif version == 3
     %HAPPE3.0
-    if EEG.srate > 500; wavLvl = 10 ;
-    elseif EEG.srate > 250 && EEG.srate <= 500; wavLvl= 9 ;
-    elseif EEG.srate <= 250; wavLvl = 8 ;
+    if EEG.srate > 500; wavLvl = 11;
+    elseif EEG.srate > 250 && EEG.srate <= 500; wavLvl= 10;
+    elseif EEG.srate <= 250; wavLvl = 9;
     end
 end
 
@@ -23,20 +28,24 @@ ThresholdRule = wavThreshold ;
 EEG.data = double(EEG.data);
 
 %% perform wavelet denoising
+artifacts = wdenoise(reshape(EEG.data, size(EEG.data, 1), ...
+    [])', wavLvl, 'Wavelet', 'coif4', 'DenoisingMethod', ...
+    'Bayes', 'ThresholdRule', ThresholdRule, 'NoiseEstimate', ...
+    'LevelDependent')' ;
 
-if version == 2
-    %HAPPE2.0
-    artifacts = wdenoise(reshape(EEG.data, size(EEG.data, 1), ...
-        [])', wavLvl, 'Wavelet', 'coif4', 'DenoisingMethod', ...
-        'Bayes', 'ThresholdRule', ThresholdRule, 'NoiseEstimate', ...
-        'LevelDependent')' ;
-elseif version == 3
-    %HAPPE3.0
-    artifacts = wdenoise(reshape(EEG.data, size(EEG.data, 1), ...
-        [])', wavLvl, 'Wavelet', 'bior6.8', 'DenoisingMethod', ...
-        'Bayes', 'ThresholdRule', ThresholdRule, 'NoiseEstimate', ...
-        'LevelDependent')' ;
-end
+% if version == 2
+%     %HAPPE2.0
+%     artifacts = wdenoise(reshape(EEG.data, size(EEG.data, 1), ...
+%         [])', wavLvl, 'Wavelet', 'coif4', 'DenoisingMethod', ...
+%         'Bayes', 'ThresholdRule', ThresholdRule, 'NoiseEstimate', ...
+%         'LevelDependent')' ;
+% elseif version == 3
+%     %HAPPE3.0
+%     artifacts = wdenoise(reshape(EEG.data, size(EEG.data, 1), ...
+%         [])', wavLvl, 'Wavelet', 'bior6.8', 'DenoisingMethod', ...
+%         'Bayes', 'ThresholdRule', ThresholdRule, 'NoiseEstimate', ...
+%         'LevelDependent')' ;
+% end
 
 %% remove artifacts from EEG data
 preEEG = reshape(EEG.data, size(EEG.data,1), []) ;
@@ -44,6 +53,7 @@ postEEG = preEEG - artifacts ;
 EEG.data = postEEG ;
 
 EEG.setname = 'wavcleanedEEG' ;
+
 
 end
 
