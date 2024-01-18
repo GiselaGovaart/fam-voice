a82605bba15543529717af88fd40a8dd67d48605
Gisela Govaart <47251666+GiselaGovaart@users.noreply.github.com>
2024-01-18 10:25:26 +0100

    dec23

--- code/R/00_priors.R
+++ code/R/00_priors.R
@@ -38,41 +38,64 @@ plot(density(data_pilot$MMR),
 mean(data_pilot$MMR)
 sd(data_pilot$MMR)
 # the data are normally distributed, the mean = 2.266946, SD = 11.74134
-# So we set the following prior (we add some uncertainty, so a larger SD):
-priors <-
-  c(
-    prior("normal(2.27, 14)", class = "b") #  informative prior on intercept & slopes: b stand for beta
-  )
-# we set a prior for the intercept and slopes. It's a prior that assumes a normal
-# distribution centered around 2.27, with an SD of 14 microvolt.
 
-######## OTHER APPRAOCH --> Better I think 
-# I think we need to set the priors rather something like this:
-priors <- c(set_prior("normal(2.27, 14)",  # sd=14 is here the SD of the distribution of the prior on mu, so how uncertain we are about the value of mu
+# let model run with default priors
+pilot_m = brm(MMR ~ 1 + TestSpeaker + (1 | Subj), 
+              data = data_pilot)
+summary(pilot_m)
+# Intercept = 2.89
+# sigma = 10.64 
+
+# let model run with weakly informative priors
+prior_p <-
+  c(
+    prior("normal(0, 30)", class = "Intercept") #  weakly informative prior on intercept 
+  , prior("normal(0, 30)", class = "b") #  weakly informative prior on slope
+)
+pilot_m = brm(MMR ~ 1 + TestSpeaker + (1 | Subj), 
+              prior = prior_p,
+               data = data_pilot)
+summary(pilot_m)
+# Intercept = 2.92
+# sigma = 10.57
+
+# So we set the following prior (we add some uncertainty, so a larger SD):
+priors <- c(set_prior("normal(2.92, 14)",  # sd=14 is here the SD of the distribution of the prior on mu, so how uncertain we are about the value of mu
                       class = "Intercept"),
-            # set_prior("normal(0, 20)",  # this is the prior on the effect for test speaker
-            #           class = "b",
-            #           coef="TestSpeaker_n"),
             set_prior("normal(0, 14)",  # this is the prior on the slope
-                       class = "b"),  
-            set_prior("normal(14, 5)",  # this is our expectation about the error in the model, so the residual noise. it's the SD of the likelihood. It's the SD of the MMR in this case
-                      # the mean of sigma = 14 means that we expect a mean SD of the MMR of 14, with and SD of the SD of 5 (that's how much uncertainty we have about the value 14)
-                      class = "sigma")) #  sigma represents the standard deviation of the response variable, mmr in this case. The standard deviation cannot be negative and it is typically a positive value. 
-                                        # Therefore, we set a prior that reflects this constraint: normal(14,5)
+                      class = "b"),  
+            set_prior("normal(0, 14)",  # this is our expectation about the error in the model, so the residual noise. it's the SD of the likelihood. It's the SD of the MMR in this case
+                      class = "sigma")) #  sigma represents the standard deviation of the response variable, mmr in this case. 
+                                        # it's a bit strange because of course, sigma cannot be smaller than zero..
+                                        # an alternative to incorporate that would be normal(14,5)
+
+# let's check the default prior for sigma in brms:
+priors_nosigma <- c(set_prior("normal(2.92, 14)",  # sd=14 is here the SD of the distribution of the prior on mu, so how uncertain we are about the value of mu
+                      class = "Intercept"),
+            set_prior("normal(0, 14)",  # this is the prior on the slope
+                      class = "b"))
+
+validate_prior(priors_nosigma, 
+               MMR ~ 1 + TestSpeaker + (1 | Subj), 
+               data = data_pilot
+)
+# prior for sigma: student_t(3, 0, 9). student_t is a bell-shaped distribution but with heavier tails than normal distribution. this one has 3 degrees of freedom
+
 
 # Visualize the priors
 dpri <- data.frame(x = seq(-50,50,by=1))
-dpri$y1 <- dnorm(dpri$x,mean=2.27,sd=14)
+dpri$y1 <- dnorm(dpri$x,mean=2.92,sd=14)
 dpri$y2 <- dnorm(dpri$x,mean=0,sd=14)
-dpri$y3 <- dnorm(dpri$x,mean=14,sd=5)
+#dpri$y3 <- dnorm(dpri$x,mean=14,sd=5)
+dpri$y3 <- dnorm(dpri$x,mean=0,sd=14)
+
 dprig <- dpri %>% gather(y1, y2, y3, key="Prior", value="Density")
 dprig$Prior <- factor(dprig$Prior)
-levels(dprig$Prior) <- c("Prior for mu\ndnorm(x, mean=2.27, sd=14)", "Prior for slope\ndnorm(x, mean=0, sd=14)", "Prior for sigma\ndnorm(x, mean=14, sd=5)")
+levels(dprig$Prior) <- c("Prior for mu\ndnorm(x, mean=2.92, sd=14)", "Prior for slope\ndnorm(x, mean=0, sd=14)", "Prior for sigma\ndnorm(x, mean=0, sd=14)")
 ggplot(data=dprig, aes(x=x, y=Density)) + geom_line() + facet_wrap(~Prior, scales="free")
 
-# Prior predictive check --------------------------------------------------------------------
 
-# Simulate some data
+# Simulate some data --------------------------------------------------------------------
 # create experimental design
 design <-
   fixed.factor("Group", levels=c("fam", "unfam")) +
@@ -85,9 +108,38 @@ dat$TestSpeaker_n <- ifelse(dat$TestSpeaker=="1", +0.5, -0.5)
 dat$Group_n <- ifelse(dat$Group=="fam", +0.5, -0.5)
 
 # simulate data
-dat$MMR <- 2.27 + 5*dat$TestSpeaker_n + rnorm(nrow(dat),0,14) # 1st part: the mean of the MMR for all groups together, 2nd part: making sure the groups are different, 3rd part: adding noise: the SD of the MMR is 14
+dat$MMR <- 2.92 + 5*dat$TestSpeaker_n + rnorm(nrow(dat),0,14) # 1st part: the mean of the MMR for all groups together, 2nd part: making sure the groups are different, 3rd part: adding noise: the SD of the MMR is 14
 
+# now add dummy values for the other variables
+dat$mumDistTrainS = NA
+dat$mumDistNovelS = NA
+dat$timeVoiceFam = NA
+dat$nrSpeakersDaily = NA
 
+dat$mumDistTrainS <- rep(abs(rnorm(nrow(dat)/2,1,2)), each=2)
+dat$mumDistNovelS <- rep(abs(rnorm(nrow(dat)/2,1,2.5)), each=2)
+dat$timeVoiceFam <- rep(round(rnorm(nrow(dat)/2,21,2)), each=2)
+dat$nrSpeakersDaily <- rep(round(rnorm(nrow(dat)/2,4,1)), each=2)
+
+(dat <- dat %>%
+    mutate(mumDistTrainS = mumDistTrainS - mean(mumDistTrainS)))
+(dat <- dat %>%
+    mutate(mumDistNovelS = mumDistNovelS - mean(mumDistNovelS)))
+(dat <- dat %>%
+    mutate(timeVoiceFam = timeVoiceFam - mean(timeVoiceFam)))
+(dat <- dat %>%
+    mutate(nrSpeakersDaily = nrSpeakersDaily - mean(nrSpeakersDaily)))
+
+# Now we add the contrasts to the factors itself: 
+### CHECK WHETHER THIS IS NECESSARY
+contrasts(dat$Group) <- c(-0.5, +0.5)
+contrasts(dat$TestSpeaker) <- c(-0.5, +0.5)
+
+### CHECK WHETHER THIS IS NECESSARY
+# dummy for centering the variables (added by G 8.11.23)
+data = data %/% mutate(cVar = Var-mean(Var))
+
+# Prior predictive check --------------------------------------------------------------------
 # Performing prior predictive simulations using brms
 pm1 <- brm(MMR ~ 1 + Group * TestSpeaker + (1 | Subj), data = dat,
            prior = priors,
@@ -106,25 +158,157 @@ GroupEffect <- colMeans(pp[dat$Group=="fam",]) - colMeans(pp[dat$Group=="unfam",
 hist(GroupEffect)
 
 # This looks good, we see that in the distribution of the mean MMR, the value that is most often found is between 0-5, and our priors said that 
-# the mean is 2,27. The SD also looks fine. Actually, this distribution looks a lot like the histogram for the actual pilot MMR data:
+# the mean is 2.92. The SD also looks fine. Actually, this distribution looks a lot like the histogram for the actual pilot MMR data:
 plot(hist(data_pilot$MMR),
      main="Pilot data",xlab="MMR")
+# also, the effect of TestSpeaker (simulated as 5) seems to be retrieved, and the non-effect of Group is also retrieved
 
+# Performing prior predictive simulations using brms with covariates
+pm2 <- brm(MMR ~ 1 + TestSpeaker * Group + 
+             mumDistTrainS * TestSpeaker + 
+             mumDistNovelS * TestSpeaker + 
+             timeVoiceFam * TestSpeaker * Group +
+             nrSpeakersDaily * TestSpeaker * Group + 
+             (1 | Subj) + (1 | TestSpeaker:Group),
+           data = dat,
+           prior = priors,
+           iter = 2000, chains = 4,
+           family = gaussian(), 
+           sample_prior = "only",
+           control = list(adapt_delta = 0.99))
+
+pp <- posterior_predict(pm2) 
+pp <- t(pp)
+# distribution of mean MMR
+hist(colMeans(pp), breaks = 40)
+# distribution of the effect of TestSpeaker
+TestSpeakerEffect <- colMeans(pp[dat$TestSpeaker=="2",]) - colMeans(pp[dat$TestSpeaker=="1",])
+hist(TestSpeakerEffect, breaks = 40)
+# distribution of the effect of Group
+GroupEffect <- colMeans(pp[dat$Group=="fam",]) - colMeans(pp[dat$Group=="unfam",])
+hist(GroupEffect)
+
+# This looks good, we see that in the distribution of the mean MMR, the value that is most often found is between 0-5, and our priors said that 
+# the mean is 2.92. The SD also looks fine. Actually, this distribution looks a lot like the histogram for the actual pilot MMR data:
+plot(hist(data_pilot$MMR),
+     main="Pilot data",xlab="MMR")
+# also, the effect of TestSpeaker (simulated as 5) seems to be retrieved, and the non-effect of Group is also retrieved
+
+# --> we see that added covariates to this check does not really change the results.
+
+
+# sensitivity analysis --------------------------------------------------------------------
+# other priors:
+priors_orig <-
+  c(set_prior("normal(2.92, 14)",  
+              class = "Intercept"),
+    set_prior("normal(0, 14)",  
+              class = "b"))
+
+priors2 <-
+  c(set_prior("normal(0, 28)",  
+                class = "Intercept"),
+      set_prior("normal(0, 28)",  
+                class = "b")) # weakly informative prior on intercept & slopes: this is still biologically plausible
+
+priors3 <-
+  c(set_prior("normal(0, 50)",  
+              class = "Intercept"),
+    set_prior("normal(0, 50)",  
+              class = "b")) # uninformative prior on intercept & slopes: this is not biologically plausible
+
+
+# Plot the priors (code adapted from https://osf.io/eyd4r/)
+# Save the x-axis limits in a dataframe
+df <- data.frame(x = c(-50, 50))
+p <- ggplot(df, aes(x=x)) +
+  # First, add the prior distribution of the original prior. 
+  # The first line creates an area (filled in with color), the second line creates a line graph
+  stat_function(fun = dnorm, n = 1001, args = list(mean = 2.92, sd = sqrt(14)), geom = "area", aes(fill = "Original Prior"), alpha = .7) +
+  stat_function(fun = dnorm, n = 1001, args = list(mean = 2.92, sd = sqrt(14))) +
+  # Repeat the above for each of the two alternative priors
+  stat_function(fun = dnorm, n = 1001, args = list(mean = 0, sd = sqrt(28)), geom = "area", aes(fill = "Alternative 1"), alpha = .7) +
+  stat_function(fun = dnorm, n = 1001, args = list(mean = 0, sd = sqrt(28))) +
+  stat_function(fun = dnorm, n = 1001, args = list(mean = 0, sd = sqrt(50)), geom = "area", aes(fill = "Alternative 2"), alpha = .7) +
+  stat_function(fun = dnorm, n = 1001, args = list(mean = 0, sd = sqrt(50))) +
+  scale_fill_brewer(palette = "Blues", name = "") +
+  ggtitle("Comparison of Priors") +
+  xlab(bquote(beta[sex])) +
+  theme_classic() +  
+  theme(legend.position="bottom", 
+        axis.title.x=element_text(family = "sans", size = 18),
+        axis.title.y=element_blank(),
+        plot.title = element_text(hjust = 0.5, family="sans", size = 18),
+        legend.text=element_text(size=14))
+# Print the plot
+p
+
+# model orig prior (converges)
+m_sens_orig <- brm(MMR ~ 1 + TestSpeaker * Group + 
+             mumDistTrainS * TestSpeaker + 
+             mumDistNovelS * TestSpeaker + 
+             timeVoiceFam * TestSpeaker * Group +
+             nrSpeakersDaily * TestSpeaker * Group + 
+             (1 | Subj) + (1 | TestSpeaker*Group),
+           data = dat,
+           prior = priors_orig,
+           iter = 4000, chains = 4, warmup = 2000, thin = 1,
+           family = gaussian(), 
+           control = list(adapt_delta = 0.99, max_treedepth = 15))
+
+# model alternative priors 2 (4 divergent transitions after warmup)
+m_sens_2 <- brm(MMR ~ 1 + TestSpeaker * Group + 
+                     mumDistTrainS * TestSpeaker + 
+                     mumDistNovelS * TestSpeaker + 
+                     timeVoiceFam * TestSpeaker * Group +
+                     nrSpeakersDaily * TestSpeaker * Group + 
+                     (1 | Subj) + (1 | TestSpeaker*Group),
+                   data = dat,
+                   prior = priors2,
+                   iter = 4000, chains = 4, warmup = 2000, thin = 1,
+                   family = gaussian(), 
+                   control = list(adapt_delta = 0.99, max_treedepth = 15))
+# model alternative priors 3 (4 divergent transitions after warmup)
+m_sens_3 <- brm(MMR ~ 1 + TestSpeaker * Group + 
+                  mumDistTrainS * TestSpeaker + 
+                  mumDistNovelS * TestSpeaker + 
+                  timeVoiceFam * TestSpeaker * Group +
+                  nrSpeakersDaily * TestSpeaker * Group + 
+                  (1 | Subj) + (1 | TestSpeaker*Group),
+                data = dat,
+                prior = priors3,
+                iter = 4000, chains = 4, warmup = 2000, thin = 1,
+                family = gaussian(), 
+                control = list(adapt_delta = 0.99, max_treedepth = 15))
+
+summary(m_sens_orig)
+summary(m_sens_2)
+summary(m_sens_3)
+
+posterior_summary(m_sens_orig, variable=c("b_Intercept","b_TestSpeaker1", "b_Group1", "sigma"))
+posterior_summary(m_sens_2, variable=c("b_Intercept","b_TestSpeaker1", "b_Group1", "sigma"))
+posterior_summary(m_sens_3, variable=c("b_Intercept","b_TestSpeaker1", "b_Group1", "sigma"))
+
+## ggf add visualization here as well, also from https://osf.io/eyd4r/)
+
+
+
+# ADD THE COVARIATES TO THE POSTERIOR CHECKS!
 
 # Posterior checks --------------------------------------------------------------------
 # Now we check whether the model can also recover the underlying data (with our simulated data)
 # define and fit model
 m1 <- brm(MMR ~ 1 + TestSpeaker_n + (1 | Subj), data = dat,
-           prior = priors,
-           iter = 2000, chains = 4, family = gaussian(), 
-           control = list(adapt_delta = 0.99))
+          prior = priors,
+          iter = 2000, chains = 4, family = gaussian(), 
+          control = list(adapt_delta = 0.99))
 # check traces and posterior distributions
 plot(m1)
 # the traces look like good hairy caterpillars
 # since we simulated the data ourselves, we know the true values that the model needs to recover, 
 # and we can check whether these distributions make sense: 
-# the simulated data was: dat$MMR <- 2.27 + 5*dat$TestSpeaker_n + rnorm(nrow(dat),0,14)
-# so the mean (intercept) should be ~2.27
+# the simulated data was: dat$MMR <- 2.92 + 5*dat$TestSpeaker_n + rnorm(nrow(dat),0,14)
+# so the mean (intercept) should be ~2.92
 # wordFreq (beta) shoud be ~ 5
 # sigma should be ~14
 # the posterior distributions look quite close to this! They are also normally distributed, and for example do not have 2 bumps
@@ -140,6 +324,11 @@ summary(m1)
 posterior_summary(m1, variable="b_TestSpeaker_n")
 
 
+
+
+
+
+### FROM HERE DOWNWARDS, THIS CAN GO TO 01_parameter_estimation.R
 # Contrast coding ---------------------------------
 
 # Here's an example of how you can estimate the effect of speaker 1 compared to speaker 4:
@@ -161,10 +350,10 @@ posterior_summary(m1, variable="b_TestSpeaker_n")
 contrasts(dat$Group) <- c(-0.5, +0.5)
 contrasts(dat$TestSpeaker) <- c(-0.5, +0.5)
 fit_Nest <- brm(MMR ~ 1 + TestSpeaker / Group,
-                 data = dat,
-                 family = gaussian(),
-                 prior = priors
-                 )
+                data = dat,
+                family = gaussian(),
+                prior = priors
+)
 
 summary(fit_Nest)
 
@@ -183,25 +372,6 @@ plot(fit_Nest)
 #The regression coefficients estimate the grand mean, the difference for the main effect of TestSpeaker and the two differences (for Group; 
 # i.e., simple main effects) within the two levels (TestSpeaker 1 and TestSpeaker 4) of TestSpeaker
 
-# now add dummy values for the other variables
-dat$mumDistTrainS = NA
-dat$mumDistNovelS = NA
-dat$timeVoiceFam = NA
-dat$nrSpeakersDaily = NA
-
-dat$mumDistTrainS <- rep(abs(rnorm(nrow(dat)/2,1,2)), each=2)
-dat$mumDistNovelS <- rep(abs(rnorm(nrow(dat)/2,1,2.5)), each=2)
-dat$timeVoiceFam <- rep(round(rnorm(nrow(dat)/2,21,2)), each=2)
-dat$nrSpeakersDaily <- rep(round(rnorm(nrow(dat)/2,4,1)), each=2)
-
-(dat <- dat %>%
-    mutate(mumDistTrainS = mumDistTrainS - mean(mumDistTrainS)))
-(dat <- dat %>%
-    mutate(mumDistNovelS = mumDistNovelS - mean(mumDistNovelS)))
-(dat <- dat %>%
-    mutate(timeVoiceFam = timeVoiceFam - mean(timeVoiceFam)))
-(dat <- dat %>%
-    mutate(nrSpeakersDaily = nrSpeakersDaily - mean(nrSpeakersDaily)))
 
 
 num_chains <- 4 # number of chains = number of processor cores
@@ -226,40 +396,11 @@ fit_Nest2 <- brm(MMR ~ 1 + TestSpeaker / Group +
                    # These are the parameters of the algorithms. Here A changed the default values (to make more precise but less fast).
                    # Check which ones are possible!
                  ),                 data = dat,
-                family = gaussian(),
-                prior = priors)
+                 family = gaussian(),
+                 prior = priors)
 
 
 pairs(fit_Nest2)
 plot(fit_Nest2)
 summary(fit_Nest2)
-
-
-
-
-# sensitivity analysis --------------------------------------------------------------------
-# other priors:
-priors1 <-
-  c(
-    prior("normal(0, 10)", class = "b") # weakly informative prior on intercept & slopes
-  )
-
-priors2 <-
-  c(
-    prior("normal(0, 28)", class = "b") # weakly informative prior on intercept & slopes
-  )
-
-priors3 <-
-  c(
-    prior("uniform(-100, 300)", class = "b") # uninformative prior on intercept & slopes
-  )
-
-
-
-"normal(0, 28)" # weakly informative. this is still plausible
-"normal(0, 50)" # uninformative. not biologically plausible
-
-
-
-
 
